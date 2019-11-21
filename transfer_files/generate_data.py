fefb9b21886e59354470d0929dc1b75dc49c8904
stephwang <stephwang@google.com>
2019-11-21 14:05:32 -0500

    Generate dummy JSON people object data

--- transfer_files/generate_data.py
+++ transfer_files/generate_data.py
@@ -1,44 +1,50 @@
+import datetime
 import json
-import uuid
 import random
+import uuid
+import lorem
+import pandas as pd
 
 
 def people_generator(length):
   for x in range(length):
     generated_name = generate_name() + ' ' + generate_name()
+    generated_company = random_pick_from_list(COMPANIES)
     yield {
         'id': generate_id(),
         'name': generated_name,
         'index': x,
         'guid': generate_id(),
         'isActive': bool(random.getrandbits(1)),
-        'balance': '',
-        'age': '',
-        'eyeColor': '',
-        'gender': '',
-        'company': '',
-        'email': '',
-        'phone': '',
-        'address': '',
-        'about': '',
-        'registered': '',
-        'latitude': 0,
-        'longitude': 0,
-        'friends':[
+        'balance': generate_balance(),
+        'age': random.randrange(5, 90, 1),
+        'eyeColor': random_pick_from_list(EYE_COLORS),
+        'gender': random_pick_from_list(GENDERS),
+        'company': generated_company,
+        'email': generated_name.split(' ')[0]+'@'+generated_company.split(' ')[0]+'.com',
+        'phone': generate_phone(),
+        'address': generate_address(),
+        'about': lorem.paragraph(),
+        'registered': generate_date(),
+        'latitude': random.SystemRandom().uniform(-100.05, 100.95),
+        'longitude': random.SystemRandom().uniform(-100.05, 100.95),
+        'friends': [
             {
                 'friendId': 0,
-                'friendName': ''
+                'friendName': generate_name() + ' ' + generate_name()
+            },
+            {
+                'friendId': 1,
+                'friendName': generate_name() + ' ' + generate_name()
+            },
+            {
+                'friendId': 2,
+                'friendName': generate_name() + ' ' + generate_name()
             }
         ],
-        'greeting': '',
-        'favoriteFruit': ''
+        'greeting': "Hello, " + lorem.sentence(),
+        'favoriteFruit': random_pick_from_list(FRUITS)
     }
-
-    # yield OrderedDict([
-    #     ('last_name', 'lastname_%i' % x),
-    #     ('first_name', 'firstname_%i' % x),
-    #     ('street_address', 'adress_%i' % x),
-    #     ('email', 'email_%i' % x)])
 
 
 def generate_id():
@@ -48,16 +54,50 @@ def generate_id():
 def generate_name():
   with open('baby_names.txt', 'r') as f:
     names = f.readlines()
-  return names[random.randrange(0, len(names)-1, 1)][:-2]
+  return names[random.randrange(0, len(names) - 1, 1)][:-2]
+
+
+def generate_balance():
+  return '$' + str(random.randrange(0, 1000000))
+
+
+def random_pick_from_list(list):
+  return random.choice(list)
+
+
+def get_snp500_companies():
+  data = pd.read_html(
+      'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
+  table = data[0]
+  company_name_col = table.iloc[:, 1]
+  return company_name_col.values.tolist()
+
+
+def generate_phone():
+  return str(random.randrange(100, 999, 1))+'-'+str(random.randrange(100, 999, 1))+'-'+str(random.randrange(100, 999, 1))
+
+
+def generate_address():
+  return str(random.randrange(1, 999, 1))+' '+str(random.randrange(4, 250, 1))+'th St '+'New York, NY '+str(random.randrange(10001,19000,1))
+
+
+def generate_date():
+  date = datetime.datetime(random.randrange(1990, 2019, 1), random.randrange(1, 12, 1), random.randrange(1, 28, 1))
+  return str(date).split(' ')[0]
 
 
 filename = 'py-generated'
-LENGTH = 3
-people_objs = people_generator(LENGTH)
+REPEAT = 5
+EYE_COLORS = ['Brown', 'Blue', 'Black', 'Hazel', 'Grey', 'Maroon', 'Red', 'Dragon']
+GENDERS = ['Female', 'Male', 'Non-binary/ third gender', 'Prefer not to say']
+COMPANIES = get_snp500_companies()
+FRUITS = ['Apple', 'Pear', 'Kiwi', 'Strawberry', 'Raspberry', 'Blackberry', 'Orange', 'Banana']
+
+people_objs = people_generator(REPEAT)
 with open('%s.json' % filename, 'w') as output:
   output.write('[')
   for obj in people_objs:
     json.dump(obj, output)
     output.write(',')
   output.write(']')
-print("Done.")
+print("You're awesome! You just made " + str(REPEAT) + " people!")
